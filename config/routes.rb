76e79c2910575b48c446d460ff58a63050375a2f
averpavel <averpavel@gmail.com>
2016-02-14 18:19:02 +0100

    add hight_voltage gem and home and about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+get 'pages/home' => 'high_voltage/pages#show', id: 'home'
+get 'pages/about' => 'high_voltage/pages#show', id: 'about'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
